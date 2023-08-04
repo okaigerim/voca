b3da999ee5fb688d45660b7f764be87bc7d9daf9
okaigerim <okaigerim21@gmail.com>
2023-08-04 22:27:32 +0600

    Upload

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>
+        <a href="/">English words</a>
+      </h1>
+
+      <div className="menu">
+        
+        <a href="#x" className="link">
+          Add a word
+        </a>
+        <a href="#x" className="link">
+          Add day
+        </a>
+      </div>
+    </div>
+  );
+}
